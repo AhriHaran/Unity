30546bcd2f55ce39c3585b8a16f7ddf414eac388
AhriHaran <53079191+AhriHaran@users.noreply.github.com>
2019-07-29 17:55:59 +0900

    2019_07_29_Seconde

--- MapManager/Assets/Scripts/PlayerControl.cs
+++ MapManager/Assets/Scripts/PlayerControl.cs
@@ -11,9 +11,15 @@ namespace Player
         public float m_ForwardSpeed = 7.0f;
         public float m_BackwardSpeed = 2.0f;
         public float m_RotateSpeed = 2.0f;
+        public float m_fComboTime = 0.7f;  //공격을 시작하면 일정 타임 내로 입력이 계속되면 콤보가 유지되고 입력이 끊기면 초기화
+        public float m_fPressTime = 0.1f;
 
         private Animator m_PlayerAnime;
         private Rigidbody m_PlayerRigid;
+        private int m_iAttackCount = 0;
+        private bool m_bCombo = false;
+        private float m_fComboCurTime = 0.0f;
+        private float m_fPressCurTime = 0.0f;
 
         // Start is called before the first frame update
         void Start()
@@ -25,7 +31,6 @@ namespace Player
         // Update is called once per frame
         void Update()
         {
-
         }
 
         private void FixedUpdate()
@@ -38,22 +43,59 @@ namespace Player
             m_PlayerAnime.SetFloat("Direction", h);
             m_PlayerAnime.speed = m_AniSpeed;   //애니메이션 속도
 
-
-            Vector3 Speed = new Vector3(0, 0, v);   //움직임
-            Speed = transform.TransformDirection(Speed);    //로컬 공간 -> 월드 공간
-
-            if(v > 0.1) //
+            if(!m_bCombo)
             {
-                Speed *= m_ForwardSpeed;    //전진
+                Vector3 Speed = new Vector3(0, 0, v);   //움직임
+                Speed = transform.TransformDirection(Speed);    //로컬 공간 -> 월드 공간
+
+                if (v > 0.1) //
+                {
+                    Speed *= m_ForwardSpeed;    //전진
+                }
+                else if (v < -0.1)
+                {
+                    Speed *= m_BackwardSpeed;   //후진
+                }
+
+                transform.localPosition += Speed * Time.deltaTime;
+                transform.Rotate(0, h * m_RotateSpeed, 0);
+                //공격시는 움직이지 않는다.
             }
-            else if(v < -0.1)
+            else
             {
-                Speed *= m_BackwardSpeed;   //후진
+                //콤보가 끊기는가?
+                m_fComboCurTime += Time.deltaTime;
+
+                if (m_fComboCurTime >= m_fComboTime) //콤보 타임 초과
+                {
+                    m_fComboCurTime = 0.0f;
+                    m_fPressCurTime = 0.0f;
+                    m_iAttackCount = 0;
+                    m_bCombo = false;
+                }
             }
 
-            transform.localPosition += Speed * Time.deltaTime;
-            transform.Rotate(0, h * m_RotateSpeed, 0);
+            if (Input.GetMouseButton(0))
+            {
+                if(m_iAttackCount == 1) //두번째 입력부터 꾸욱 눌렀다가 때야지 콤보 유지
+                {
+                    m_fPressCurTime += Time.deltaTime;
+                }
+            }
 
+            if(Input.GetMouseButtonUp(0))
+            {
+                if((m_fPressCurTime >= m_fPressTime && m_iAttackCount == 1) || m_iAttackCount == 0)
+                    m_iAttackCount++;
+                else if(m_iAttackCount >= 2)
+                    m_iAttackCount = 1;
+
+                m_bCombo = true;
+                m_fComboCurTime = 0.0f;
+                m_fPressCurTime = 0.0f;
+            }
+
+            m_PlayerAnime.SetInteger("AttackCount", m_iAttackCount);
         }
     }
 }
