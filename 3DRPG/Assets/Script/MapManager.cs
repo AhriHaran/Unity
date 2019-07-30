6c9d31969a68f761383d62e507e3028a43e3faaf
AhriHaran <53079191+AhriHaran@users.noreply.github.com>
2019-07-30 17:44:54 +0900

    2019_07_30_Seconde

--- 3DRPG/Assets/Script/MapManager.cs
+++ 3DRPG/Assets/Script/MapManager.cs
@@ -1,78 +1,84 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.AI;
 
 public class MapManager : MonoBehaviour
 {
     //맵의 네비 매쉬를 설정하고 맵의 클리어 조건들을 설정한다.
-    List<Vector3> m_ListVec = new List<Vector3>();    //맵에 존재하는 모든 조건 백터
-    List<string> m_ListStr = new List<string>();    //맵의 타입등의 조건
-
+    public NavMeshSurface m_surfaces;
+    public List<Vector3> m_ListEventPos = new List<Vector3>();
     string m_strStage;
+    string m_strType;
+    float m_fStageTime = 0.0f;
 
     // Use this for initialization
     void Start()
     {
-        GameObject BackGround = transform.GetChild(0).gameObject;
         m_strStage = GameManager.instance.ReturnStage(); //첫시작 
         string strFile = "Excel/" + m_strStage + "Stage_Map";
 
        List<Dictionary<string, object>> tmp = ExcelLoad.Read(strFile);
         //리스트로 저장 하고 해당 리스트에 맞춰서 맵 배치
         Vector3 vecPos = new Vector3();
-        Vector3 vecRot = new Vector3();
+        Quaternion QueRot = new Quaternion();
         Vector3 vecSca = new Vector3();
+
         for (var i = 0; i < tmp.Count; i++)
         {
             string strText = "Prefabs/BackGround/" + tmp[i]["Prefab"].ToString();   //경로
             vecPos.Set(float.Parse(tmp[i]["LocX"].ToString()), float.Parse(tmp[i]["LocY"].ToString()), float.Parse(tmp[i]["LocZ"].ToString()));   //좌표
-            vecRot.Set(float.Parse(tmp[i]["RotX"].ToString()), float.Parse(tmp[i]["RotY"].ToString()), float.Parse(tmp[i]["RotZ"].ToString()));   //회전
+            QueRot.Set(float.Parse(tmp[i]["QuaX"].ToString()), float.Parse(tmp[i]["QuaY"].ToString()), float.Parse(tmp[i]["QuaZ"].ToString()), float.Parse(tmp[i]["QuaW"].ToString()));   //회전
             vecSca.Set(float.Parse(tmp[i]["ScaleX"].ToString()), float.Parse(tmp[i]["ScaleY"].ToString()), float.Parse(tmp[i]["ScaleZ"].ToString()));   //스케일
 
-            GameObject MapObject = ResourceLoader.CreatePrefab(strText, BackGround.transform);
+            GameObject MapObject = ResourceLoader.CreatePrefab(strText, transform);
 
-            MapObject.transform.SetPositionAndRotation(vecPos, Quaternion.Euler(vecRot));   //맵의 오브젝트
+            MapObject.transform.SetPositionAndRotation(vecPos, QueRot);   //맵의 오브젝트
             MapObject.transform.localScale = vecSca;
         }
         //맵 셋팅 
-        strFile = "Excel/" + m_strStage + "Stage_Condition";
-        tmp = ExcelLoad.Read(strFile);  //맵의 클리어 조건, 포지션에 의한 것
-        /*
-         * 플레이어의 첫 시작과 끝 포인트
-         * 플레이어가 해당 구역에 들어오면 특정 적 리스폰
-         * 등등 맵의 조건 중 포지션에 대한 조건
-        */
+
+        //맵 타입 설정
+        strFile = "Excel/" + m_strStage + "Stage_Type";
+        tmp = ExcelLoad.Read(strFile);
+        //맵의 타입, 시간
+
+        m_strType = tmp[0]["Type"].ToString();  //맵 타입
+        if (tmp[0]["Time"].ToString() == "NULL")
+            m_fStageTime = 0.0f;
+        else
+            m_fStageTime = float.Parse(tmp[0]["Time"].ToString());
         
-        for(var i = 0; i < tmp.Count; i++)
+        m_surfaces.BuildNavMesh();
+
+        strFile = "Excel/" + m_strStage + "Stage_Event_Pos";
+        tmp = ExcelLoad.Read(strFile);
+        //맵의 이벤트 포스
+
+        //            Index LocX    LocY LocZ
+        for (var i =0; i < tmp.Count; i++)
         {
-            Vector3 Vec = new Vector3();
-            string strX = i + "ConLocX";
-            string strY = i + "ConLocY";
-            string strZ = i + "ConLocZ";
-            Vec.Set(float.Parse(tmp[i][strX].ToString()), float.Parse(tmp[i][strY].ToString()), float.Parse(tmp[i][strZ].ToString()));
-            m_ListVec.Add(Vec);
+            Vector3 Node = new Vector3(float.Parse(tmp[i]["LocX"].ToString()), float.Parse(tmp[i]["LocY"].ToString()), float.Parse(tmp[i]["LocZ"].ToString()));
+
+            GameObject Object = new GameObject();
+            Object.name = (i.ToString() +"EventPos");
+            Object.transform.SetParent(transform);
+            Object.transform.position = Node;
+
+            m_ListEventPos.Add(Node);
         }
-
-        /*
-         * 맵의 상태
-         * 웨이브 형식이거나
-         * 몇 마리의 몹을 제거하거나
-         * 시간 제한 동안 살아남거나
-         * 시간 제한 내에 모든 적을 제거하는 등의 컨셉
-         */
-
-        //
-            
-    }
-
-    public List<Vector3> ReturnPos()
-    {
-        return m_ListVec;   //벡터 리스트를 반환 받는다.
+        //특정 이벤트를 위한 포지션 벡터
+        
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    public List<Vector3> ReturnEventPos()
+    {
+        return m_ListEventPos;
     }
 }
